adfd2408cd84c1aad7b2167be8db721a909002d8
Victor Hurdugaci <victor.hurdugaci@microsoft.com>
2014-12-08 12:09:24 -0800

    Add the methods from ActivatorUtilities as ITypeActivator extension methods

--- src/Microsoft.Framework.DependencyInjection/TypeActivatorExtensions.cs
+++ src/Microsoft.Framework.DependencyInjection/TypeActivatorExtensions.cs
@@ -9,12 +9,39 @@ namespace Microsoft.Framework.DependencyInjection
     {
         public static T CreateInstance<T>(this ITypeActivator activator, IServiceProvider serviceProvider, params object[] parameters)
         {
+            return (T)CreateInstance(activator, serviceProvider, typeof(T), parameters);
+        }
+
+        public static T GetServiceOrCreateInstance<T>(this ITypeActivator activator, IServiceProvider services)
+        {
+            return (T)GetServiceOrCreateInstance(activator, services, typeof(T));
+        }
+
+        public static object GetServiceOrCreateInstance(this ITypeActivator activator, IServiceProvider services, Type type)
+        {
+            return GetServiceNoExceptions(services, type) ?? CreateInstance(activator, services, type);
+        }
+
+        private static object GetServiceNoExceptions(IServiceProvider services, Type type)
+        {
+            try
+            {
+                return services.GetService(type);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private static object CreateInstance(this ITypeActivator activator, IServiceProvider serviceProvider, Type serviceType, params object[] parameters)
+        {
             if (activator == null)
             {
-                throw new ArgumentNullException("activator");
+                throw new ArgumentNullException(nameof(activator));
             }
 
-            return (T)activator.CreateInstance(serviceProvider, typeof(T), parameters);
+            return activator.CreateInstance(serviceProvider, serviceType, parameters);
         }
     }
 }
